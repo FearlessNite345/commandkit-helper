60d4bc9a0f654b290504a42cb7c08eddf81da2b7
FearlessNite345 <110247392+FearlessNite345@users.noreply.github.com>
2023-10-17 20:04:28 -0400

    🐛 FIX: Export issue

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './typings/CommandTypes';
 export * from './classes/CommandBuilders';
 export * from './classes/ValidationBuilders'
 export * from './typings/ValidationTypes'
+export * from './classes/ExtendedClient'
